9ff94a2779026a0ccab069ee51faeb6d004abfe7
bangkiki <121254801+bangkiki@users.noreply.github.com>
2022-12-23 01:18:10 +0700

    Add files via upload

---
+++ java.js
@@ -0,0 +1,24 @@
+var slideIndex = 0;
+
+function showSlides() {
+	var i;
+	var slides = document.getElementsByClassName("slide");
+	for (i = 0; i < slides.length; i++) {
+		slides[i].style.display = "none";
+	}
+	slideIndex++;
+	if (slideIndex > slides.length) {
+		slideIndex = 1;
+	}
+	slides[slideIndex-1].style.display = "block";
+	setTimeout(showSlides, 3000); // Ganti gambar setiap 3 detik
+}
+
+showSlides();
+<button id="myBtn">Klik Saya</button>
+
+
+	document.getElementById("myBtn").addEventListener("click", function() {
+		alert("Tombol Diklik!");
+	});
+
